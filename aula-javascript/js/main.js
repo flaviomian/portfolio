7f87d2e4d5f98b61d31d719964bb21470fd9715f
Flavio Mian <f.mian@live.com>
2021-03-25 22:01:27 -0300

    commit on 'main'

---
+++ aula-javascript/js/main.js
@@ -0,0 +1,64 @@
+function clique() {
+    document.getElementById("agradecer").innerHTML = "Obrigado por Clicar";
+}
+
+function trocar() {
+    document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse."
+}
+
+function voltar() {
+    document.getElementById("mousemove").innerHTML = "Passe o mouse aqui."
+}
+
+function alterar() {
+
+}
+
+/*LISTA
+var lista = [nome: “rosa”, “cravo”, “tulipa”, “margarida”]
+console.log(lista[2]);*/
+
+/*Data:
+var d = new Date();
+alert(d)
+*/
+
+/*ESTRUTURA DE REPETIÇÃO FOR
+var count;
+for (count = 0; count <= 5; count++) {
+    console.log(count)
+}
+*/
+
+
+/*Estrutura de Repetição While
+var count = 0;
+while (count < 5) {
+    console.log(count);
+    count++
+};
+*/
+
+/*Condicionais:
+var idade = prompt("Qual sua idade?")
+if (idade >= 18) {
+    alert("Maior de idade");
+} else {
+    alert("Menor de idade")
+}
+*/
+
+/* var nome = "Flavio Mian"
+    //alert('Mensagem em JavaScript: Olá,' + nome)
+console.log(nome); */
+
+//var lista = ["item a", "item b", "item c"]
+
+//apresenta o item "0" da lista
+//console.log(lista[0]) 
+
+/* adiciona um item ao final
+lista.push("item d")
+console.log(lista) */
+
+//Lista: var fruta = [{ nome: "maçã", cor: "vermelha" }, { nome: "uva", cor: "roxa" }]
