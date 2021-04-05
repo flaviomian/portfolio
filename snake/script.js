352115dc2f518f8e7a95f9f719b05fdcdc940e6a
Flavio Mian <f.mian@live.com>
2021-04-05 20:51:28 -0300

    ciar comida aleatoria no mapa

--- snake/script.js
+++ snake/script.js
@@ -8,6 +8,11 @@ snake[0] = {
 }
 let direction = "right";
 
+let comida = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
+
 function criarBG() {
     context.fillStyle = "lightgreen";
     context.fillRect(0, 0, 16 * box, 16 * box);
@@ -20,6 +25,11 @@ function criarCobra() {
         context.fillStyle = "green";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
+}
+
+function criarComida() {
+    context.fillStyle = "red";
+    context.fillRect(comida.x, comida.y, box, box);
 }
 
 document.addEventListener('keydown', update);
@@ -40,6 +50,7 @@ function play() {
 
     criarBG();
     criarCobra();
+    criarComida();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
