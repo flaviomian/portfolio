df853692cf2ede922f576c9bcb0ca19f0dcdcb62
Flavio Mian <f.mian@live.com>
2021-04-05 21:09:14 -0300

    criar regra para interromper jogo em caso de choque

--- snake/script.js
+++ snake/script.js
@@ -47,6 +47,12 @@ function play() {
     if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if (snake[0].y < 0 && direction == "up") snake[0].y = 16 * box
 
+    for (i = 1; i < snake.length; i++) {
+        if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
+            clearInterval(jogo);
+            alert('Fim de Jogo');
+        }
+    }
 
     criarBG();
     criarCobra();
@@ -68,8 +74,6 @@ function play() {
         comida.y = Math.floor(Math.random() * 15 + 1) * box;
     }
 
-
-
     let newHead = {
         x: snakeX,
         y: snakeY
@@ -79,5 +83,3 @@ function play() {
 }
 
 let jogo = setInterval(play, 100);
-
-play()
