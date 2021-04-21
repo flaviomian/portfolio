6c0304afa65c95d96c4375046a141737e3fd358c
Flavio Mian <78768938+flaviomian@users.noreply.github.com>
2021-04-21 18:58:36 -0300

    Add files via upload

---
+++ curso-em-video/javascript/aula13/doWhile.js
@@ -0,0 +1,9 @@
+//Estrutura de repetiçao com teste lógico no final
+
+var c = 1 //contador
+do { //faça o bloco à seguir:
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6) // enquanto c for menor ou igual a 6
+
+//Quando c for maior que 6, o loop acaba
---
+++ curso-em-video/javascript/aula14/for.js
@@ -0,0 +1,15 @@
+//Estrutura de Repetição com Variável de Controle (for)
+
+/*
+for(inicio;teste logico; incremento){
+    bloco que vai se repitir enquanto o teste lógico for verdadeiro.
+}
+
+*/
+console.log('Início')
+
+for (var c = 1; c <= 4; c++) {
+    console.log(`Passo ${c}`)
+}
+
+console.log('Fim')
