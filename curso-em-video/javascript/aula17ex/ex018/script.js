0e4a564f7d3170d110794b8234427ad286707bfb
Flavio Mian <f.mian@live.com>
2021-04-25 16:32:08 -0300

    update ex18

--- curso-em-video/javascript/aula17ex/ex018/script.js
+++ curso-em-video/javascript/aula17ex/ex018/script.js
@@ -1,34 +1,63 @@
 let lis = []
+let num = document.getElementById('numtxt')
+let listanum = document.getElementById('listanum')
+let res = document.getElementById('res')
 
-function addnum() {
-    let num = document.getElementById('numtxt')
-    let listanum = document.getElementById('listanum')
-
-    if (num.value > 100 || num.value <= 0 || num.value == NaN) {
-        alert('Numero digitado inválido.')
+function isNumber(n) {
+    if (Number(n) >= 1 && Number(n) <= 100) {
+        return true
     } else {
-        lis.push(Number(num.value))
-        let item = document.createElement('option')
-        item.text = `Valor ${num.value} adicionado.`
-        listanum.appendChild(item)
-        console.log(lis)
-        console.log(lis.length)
-        lis.sort()
-
+        return false
     }
 }
 
+function inLista(n, l) {
+    if (l.indexOf(Number(n)) != -1) {
+        return true
+    } else {
+        return false
+    }
+}
 
+function addnum() {
+
+    if (isNumber(num.value) && !inLista(num.value, lis)) {
+        lis.push(Number(num.value))
+        let item = document.createElement('option')
+        item.setAttribute('id', 'newobj')
+        item.text = `Valor ${num.value} adicionado.`
+        listanum.appendChild(item)
+        res.innerHTML = ''
+    } else {
+        alert('Numero digitado inválido ou já registrado.')
+    }
+    num.value = ""
+    num.focus()
+}
 
 
 function resultado() {
-    let res = document.getElementById('res')
-    let soma = 0
-    for (let c = 0; c < lis.length; c++) {
-        soma += lis[c]
+    if (lis.length == 0) {
+        res.innerHTML = 'Adicione valores para verificar.'
+    } else {
+        let soma = 0
+        let media = 0
+        let maior = lis[0]
+        let menor = lis[0]
+
+        for (let pos in lis) {
+            soma += lis[pos]
+            if (lis[pos] > maior)
+                maior = lis[pos]
+            if (lis[pos] < menor)
+                menor = lis[pos]
+        }
+        media = soma / lis.length
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo foram adicionados ${lis.length} números.</p>`
+        res.innerHTML += `<p>O menor número adicionado foi ${menor}.</p>`
+        res.innerHTML += `<p>O maior número adicionado foi ${maior}.</p>`
+        res.innerHTML += `<p>A soma de todos os valores é ${soma}.</p>`
+        res.innerHTML += `<p>A média de todos os valores é ${media.toFixed(2)}.</p>`
     }
-    res.innerHTML = `Ao todo foram adicionados ${lis.length} números.<br>`
-    res.innerHTML += `O menor número adicionado foi ${lis[0]}.<br>`
-    res.innerHTML += `O maior número adicionado foi ${Math.max.apply(null, lis)}.<br>`
-    res.innerHtml += `A soma de todos os valores é ${soma}`
 }
